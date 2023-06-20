33de9de0496306963717684e3ef2086e7898d30f
Samar Saleh <42183549+samar897@users.noreply.github.com>
2023-06-21 01:52:53 +0300

    Update Pending.jsx

--- Foodfree/src/Components/Pending.jsx
+++ Foodfree/src/Components/Pending.jsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import {
   // Box,
   TableContainer,
@@ -7,69 +8,83 @@ import {
   Th,
   Tr,
   Tbody,
-  Td,
+  // Td,
   Table,
-  Button,
-  ButtonGroup,
+  // Button,
+  // ButtonGroup,
   Box
 } from "@chakra-ui/react";
 import PendingRow from './PendingRow';
-import axios from 'axios';
-
 
 function Pending() {
 
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
 
-  const fetchData = async () => {
-    const header = `Authorization:Bearer ${localStorage.getItem('token')}`
-    try {
-      const response = await axios.get('https://food-free.onrender.com/FoodFreeRouter/ListFoodfree', {
-        headers: header
-      });
-      console.log(response);
-      if(!response.data.errorMessage){
-        setData(response.data);
-      }
-      
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
 
-  return (
-    <>
-      <Box>
-        <TableContainer className="table">
-          <Table>
-            <Thead>
-              <Tr>
-                <Th>اسم المنتج</Th>
-                <Th>رقم المنتج</Th>
-                <Th>الإجراء</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-            {data.map((item, index) => (
-              <div>
-                <PendingRow
-                  key={index}
-                  productName={item.id}
-                  productNumber={item.name}
-                />
-              </div>
+  console.log(localStorage.getItem("token"));
 
-          ))}
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Box>
-    </>
-  );
+   // Redirect to the other page.
+   const url = "http://localhost:8000/FoodFreeRouter/ListFoodfree"
+ 
+   React.useEffect(() => {
+      axios.get(url,{
+        headers: {
+       
+         authorization: 'Bearer '+  localStorage.getItem("token"),
+        
+       },
+      }).then(res=>{
+
+        setData(res.data.FoodFreeData);
+         
+       // console.log(res.data);
+        console.log(res.data);
+        console.log(res.data.FoodFreeData[0]._id+" c");
+
+        localStorage.getItem("FullUserName")
+        localStorage.getItem("id")
+      })
+  
+    }, [])
+ 
+
+
+    return (
+      <>
+        <Box>
+          <TableContainer className="table">
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th>اسم المنتج </Th>
+                  <Th>وصف المنتج</Th>
+                  <Th>هل يسبب حساسية</Th>
+                  <Th>نوع المنتج</Th>
+                  <Th>الإجراء</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+
+              {data.map((item) => (
+                
+                  <PendingRow
+                    key={item.id}
+                    id={item._id}
+                    productName={item.Food_Free_Name}
+                    FoodDescription={item.FoodDescription}
+                    FoodType={item.FoodType}
+                    AllergyStatus={item.AllergyStatus}
+                    
+                  />
+            ))}
+            
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </>
+    );
 }
 
 export default Pending;
